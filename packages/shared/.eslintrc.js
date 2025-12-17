c1da42c77f6b5e688193bf0085173cf6434fa14c
brenocidade <brenocidade01@gmail.com>
2025-12-16 21:12:14 -0300

    Refactor code structure for improved readability and maintainability

---
+++ packages/shared/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  root: true,
+  env: {
+    node: true,
+    es2020: true,
+  },
+  ignorePatterns: [".eslintrc.js", "dist/**", "node_modules/**", "**/*.d.ts"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
